ddc36186c9b6b844a2e837365857ef16860ba561
virajk <virajk@codegen.net>
2020-09-21 11:48:43 +0530

    predict api

---
+++ v2/api.py
@@ -0,0 +1,31 @@
+import json
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+app = Flask(__name__)
+
+from tensorflow import keras
+model = keras.models.load_model("saved_model/my_model")
+
+@app.route('/')
+def test_get():
+    return 'api working!'
+
+@app.route('/predict', methods=['POST', 'GET'])
+def test_post():
+    if request.method == 'POST':
+        pred_dicts = request.get_json()
+        df = dict(pd.DataFrame(pred_dicts))
+
+        pred_ds1 = tf.data.Dataset.from_tensor_slices(df)
+        pred_ds1 = pred_ds1.batch(32)
+
+        model_predicts = model.predict(pred_ds1)
+
+        predicts = [np.argmax(predict_item).item() for predict_item in model_predicts]
+
+    return jsonify(data={'predictions': predicts})
