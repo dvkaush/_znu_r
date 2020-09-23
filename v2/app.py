1ca02be65dcb4fa4a13bb0cfdfb6c28c854573e4
virajk <virajk@codegen.net>
2020-09-24 02:41:25 +0530

    tt5

--- v2/app.py
+++ v2/app.py
@@ -1,15 +1,15 @@
 import json
 import numpy as np
 import pandas as pd
-# import tensorflow as tf
+import tensorflow as tf
 from flask import Flask
-# from flask import request
-# from flask import jsonify
+from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
 
-# from tensorflow import keras
-# model = keras.models.load_model("saved_model/my_model")
+from tensorflow import keras
+model = keras.models.load_model("saved_model/my_model")
 
 @app.route('/')
 def test_get():
@@ -22,11 +22,13 @@ def test_post():
         df = dict(pd.DataFrame(pred_dicts))
         print(df)
 
-        # pred_ds1 = tf.data.Dataset.from_tensor_slices(df)
-        # pred_ds1 = pred_ds1.batch(32)
+        pred_ds1 = tf.data.Dataset.from_tensor_slices(df)
+        pred_ds1 = pred_ds1.batch(32)
 
-        # model_predicts = model.predict(pred_ds1)
+        model_predicts = model.predict(pred_ds1)
 
-        # predicts = [np.argmax(predict_item).item() for predict_item in model_predicts]
+        predicts = [np.argmax(predict_item).item() for predict_item in model_predicts]
 
     return jsonify(data={'predictions': 10})
+
+app.run(host='0.0.0.0', port=8000)
